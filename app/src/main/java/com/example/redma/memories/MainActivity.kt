c73ba262118f21415564ab6caddbc5386e997d9e
GaZi-NK <redman.fox@icloud.com>
2020-02-12 07:22:23 +0900

    とりあえず表示させることはできた 一行文が画面全体になっているのでいいようにしないと

--- app/src/main/java/com/example/redma/memories/MainActivity.kt
+++ app/src/main/java/com/example/redma/memories/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.redma.memories
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.recyclerview_layout.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,21 +15,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //アダプターのインスタンスを作成
-        var adapter = RecyclerViewAdapter(createDataset())
-
-        //レイアウトマネージャーのインスタンスを取得
-        var lim = LinearLayoutManager(this)
-
+        var adapter = RecyclerViewAdapter(createDataset())    //アダプターのインスタンスを作成
+        var lim = LinearLayoutManager(this)    //レイアウトマネージャーのインスタンスを取得
+        //表示するデータの数が固定長であれば、リソースを有効活用し、パフォーマンスを向上させる設定。
         MemoriesView.setHasFixedSize(true)
-
+        //レスポンシブにデータを表示するために使用するLayoutManagerを設定
         MemoriesView.layoutManager = lim
-
         //RecyclerView(MemoriewView)にアダプターをセット
         MemoriesView.adapter = adapter
 
+        //スタートボタン押下時の動作
+        //TODO ①startbtn.setOnClickListener{}
+
+        //編集ボタンクリックで編集画面へ
+        //adapter.setOnItemClickListener{
+        }
+
+        //削除ボタン押下時の動作
+        // TODO ②deletebtn.setOnClickListener{}
 
     }
+
+
     private fun createDataset() : MutableList<MemoryData> {
         var  list : MutableList<MemoryData> = mutableListOf()
         for (i in 0..50){
@@ -36,4 +46,3 @@ class MainActivity : AppCompatActivity() {
         }
         return list
     }
-}
